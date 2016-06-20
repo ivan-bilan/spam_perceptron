f15cd5e45ea42b2587b889454b0e66d4ba0bf00d
bilan <bilan@cip.ifi.lmu.de>
2016-06-20 15:40:34 +0200

    changed threshold

--- perceptron_utils.py
+++ perceptron_utils.py
@@ -1,15 +1,21 @@
 # Code shared for perceptron training and testing.
 import re
 
-threshold = 0.5
+threshold = 0.6
 
 def dot_product(features, weights):
   return sum(weights[f] for f in features)
 
 def tokens(filename):
+  """
+  Read feature tokens
+  Better results are achieved when splitting on
+  whitespaces ("\s+") and not on non characters ("\W+")
+  """
   with open(filename, 'r') as myfile:
     text=myfile.read().strip().lower()
-  return re.split("\W+", text)
+
+  return re.split("\s+", text)
 
 def prediction(features, weights):
   return dot_product(features, weights) > threshold
